0810f814d030c1e8cce7825de4716d3ec649ad37
chenwenlongbug <307289940@qq.com>
2017-09-26 10:55:59 +0800

    修改配置文档

--- include/database.inc.php
+++ include/database.inc.php
@@ -1,22 +1,9 @@
 <?php
 define ('DB_TYPE','mysql');
-define ('DB_HOST','localhost');
-if(1) { //Fred
+define ('DB_HOST','192.168.0.107');
 define ('DB_USER','root');
-define ('DB_PWD','111111');
-$s=isset($_GET['dname']) ? 'qis': 'secbc';
-if($s=='secbc'){
-	define ('DB_NAME','secbc');
-}else{
-	define ('DB_NAME','qis');
-}
-}else { //Zdm
-define ('DB_USER','lq');
-define ('DB_PWD','123');
-define ('DB_NAME','secbc');
-}
-
-//define ('DB_CHARSET','GB2312');
+define ('DB_PWD','HuaHui@111111');
+define ('DB_NAME','vientiane');
 define ('DB_CHARSET','UTF8');
-define ('DB_PREFIX','bc_');
+define ('DB_PREFIX','com_');
 ?>
